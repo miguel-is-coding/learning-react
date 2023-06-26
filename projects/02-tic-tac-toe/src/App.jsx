f6ca5541696d0a297237ee1814964cbb836d05f3
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-06-26 19:18:50 +0100

    ref: extracted components and logic from app

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,61 +1,24 @@
 import './App.css'
 import {useState} from "react"
+import confetti from "canvas-confetti"
+import {Cell} from "./components/Cell.jsx";
+import {TURNS} from "./constants.js";
+import {checkEndGame, checkWinnerFrom} from "./logic/board.js";
+import {WinnerModal} from "./components/WinnerModal.jsx";
 
-const TURNS = {
-    cross: 'x',
-    circle: 'o'
-}
 
-
-const Cell = ({children, isSelected, updateBoard, index}) => {
-    const className = `square ${isSelected ? 'is-selected' : ''}`
-
-    const handleClick = () => {
-        updateBoard(index)
-    }
-
-    return (
-        <div onClick={handleClick} className={className}>
-            {children}
-        </div>
-    )
-}
-
-const WINNER_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
 
 function App() {
     const [board, setBoard] = useState(Array(9).fill(null))
     const [turn, setTurn] = useState(TURNS.cross)
     const [winner, setWinner] = useState(null)
 
-    const checkWinner = (boardToCheck) => {
-        for (const combo of WINNER_COMBOS) {
-            const [a, b, c] = combo
-            if (
-                boardToCheck[a] &&
-                boardToCheck[a] === boardToCheck[b] &&
-                boardToCheck[a] === boardToCheck[c]
-            ) {
-                return boardToCheck[a]
-            }
-        }
-        return null
-    }
-
     const resetGame = () => {
         setBoard(Array(9).fill(null))
         setTurn(TURNS.cross)
         setWinner(null)
     }
+
     const updateBoard = (index) => {
         if (board[index] || winner) {
             return
@@ -66,25 +29,29 @@ function App() {
         setBoard(newBoard)
         const newTurn = turn === TURNS.cross ? TURNS.circle : TURNS.cross
         setTurn(newTurn)
-        const newWinner = checkWinner(newBoard)
+        const newWinner = checkWinnerFrom(newBoard)
         if (newWinner) {
+            confetti()
             setWinner(newWinner)
+        } else if (checkEndGame(newBoard)) {
+            setWinner(false);
         }
     }
 
     return (
         <main className="board">
             <h1>Tic tac toe</h1>
+            <button onClick={resetGame}>Reiniciar partida</button>
             <section className="game">
                 {
-                    board.map((_, index) => {
+                    board.map((cell, index) => {
                         return (
                             <Cell
                                 key={index}
                                 index={index}
                                 updateBoard={updateBoard}
                             >
-                                {board[index]}
+                                {cell}
                             </Cell>
                         )
                     })
@@ -100,29 +67,7 @@ function App() {
                 </Cell>
             </section>
 
-            {
-                winner !== null && (
-                    <section className="winner">
-                        <div className="text">
-                            <h2>
-                                {
-                                    winner === false
-                                        ? 'Empate'
-                                        : 'Ganó:'
-                                }
-                            </h2>
-
-                            <header className="win">
-                                {winner && <Cell>{winner}</Cell>}
-                            </header>
-
-                            <footer>
-                                <button onClick={resetGame}>Empezar de nuevo</button>
-                            </footer>
-                        </div>
-                    </section>
-                )
-            }
+            <WinnerModal resetGame={resetGame} winner={winner}/>
         </main>
     )
 }
